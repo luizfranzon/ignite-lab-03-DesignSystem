77db59d2d5a751d4f093d9f941dd98eb014ee076
Luiz <luizfranzon05@hotmail.com>
2022-10-13 15:23:41 -0300

    changed default storybook brandImage

---
+++ App/.storybook/themeTest.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "My custom storybook",
+  brandUrl: "https://example.com",
+  brandImage: "https://raw.githubusercontent.com/luizfranzon/ignite-lab-03-DesignSystem/b2cffd2b905b3b3853c9e7cf39e38eac99a238d5/App/.storybook/brandImage.svg",
+  brandTarget: "_self",
+});
