45b02dc8dc3a54344d35ab03b7059ecab7363731
Luiz <luizfranzon05@hotmail.com>
2022-10-13 16:57:24 -0300

    Readme

--- .storybook/themeTest.js
+++ .storybook/themeTest.js
@@ -3,7 +3,7 @@ import { create } from "@storybook/theming";
 export default create({
   base: "dark",
   brandTitle: "My custom storybook",
-  brandUrl: "https://example.com",
+  brandUrl: "https://luizfranzon.github.io/ignite-lab-03-DesignSystem/",
   brandImage: "https://raw.githubusercontent.com/luizfranzon/ignite-lab-03-DesignSystem/b2cffd2b905b3b3853c9e7cf39e38eac99a238d5/App/.storybook/brandImage.svg",
   brandTarget: "_self",
 });
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ignite-lab-03
+<br>
+<p align="center">
+  <img width="600" src="./.github/assets/ignite-lab-react-logo.svg">
+</p>
+
+<br><br>
+<h3 align="center">Projeto realizado na 3° edição do Ignite lab, criando um design system do figma ao React.</=>
+
+<br><br>
+
+<p align="center">
+  <img width="700" src="./.github/assets/screenshot.png">
+</p>
+
+
+
