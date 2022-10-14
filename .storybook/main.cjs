1403088a8a42bab9842637373a2107c027a91f2a
Luiz <luizfranzon05@hotmail.com>
2022-10-14 13:34:39 -0300

    Aula After

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,26 +1,25 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
+    interactionsDebugger: true,
   },
   viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-03-DesignSystem'
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-lab-03-DesignSystem";
     }
 
-    return config
-  }
-}
+    return config;
+  },
+  staticDirs: ["../public"],
+};
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,89 @@
+import { FormEvent, useState } from "react";
+
+import axios from "axios";
+
+import { Checkbox } from "../components/Checkbox";
+import { Envelope, Lock } from "phosphor-react";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { TextInput } from "../components/TextInput";
+import { Text } from "../components/Text";
+import { Logo } from "../Logo";
+
+export function SignIn() {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  async function handleSignIn(event: FormEvent) {
+    event.preventDefault();
+
+    await axios.post("/sessions", {
+      email: "luizfranzon05@hotmail.com",
+      password: "123456798",
+    });
+
+    setIsUserSignedIn(true);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100 flex-col">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form onSubmit={handleSignIn} className="flex flex-col gap-4 items-stretch w-full max-w-[400px] mt-10">
+        {isUserSignedIn && <Text>Login realizado com sucesso!</Text>}
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de email</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input type="email" id="email" placeholder="Digite seu e-email" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input type="password" id="password" placeholder="************" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200 cursor-pointer select-none">
+            Lembrar de mim por 30 dias
+          </Text>
+        </label>
+
+        <Button type="submit" className="mt-4 ">
+          Entrar na plataforma
+        </Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text size="sm" asChild>
+          <a href="#" className="text-gray-400 underline hover:text-gray-200 transition-colors">
+            Esqueceu sua senha
+          </a>
+        </Text>
+        <Text size="sm" asChild>
+          <a href="#" className="text-gray-400 underline hover:text-gray-200 transition-colors">
+            Não possui conta? Crie uma agora
+          </a>
+        </Text>
+      </footer>
+    </div>
+  );
+}
